9125db42e56048f3dae5fe77b73230c9390782db
Darshit Patel <Darshit.Patel@phe.gov.uk>
2022-02-09 18:46:22 +0000

    DP - updated deploypowershell.ps1 file!

--- deployPowerShell.ps1
+++ deployPowerShell.ps1
@@ -8,8 +8,8 @@ if ($eventData.subject -match 'microsoft.compute/virtualmachines') {
 
     Connect-AzAccount -Identity
 
-    $storageAccountName = Get-AutomationVariable "saukhsaukspreapol01"
-    $resourceGroupName = Get-AutomationVariable "RG-UKHSA-UKS-PRE-AVD-APOL-01"
+    $storageAccountName = "saukhsaukspreapol01"
+    $resourceGroupName = "RG-UKHSA-UKS-PRE-AVD-APOL-01"
 
     $ctx = (Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $storageAccountName).Context
 
